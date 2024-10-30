365ad6e7ae9bd9bc6439851463cbc069c764aed2
LauraMinuti77 <168583359+LauraMinuti77@users.noreply.github.com>
2024-10-30 13:35:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Futebol',
+    'Qual clube brasileiro é o maior vencedor da Libertadores da América?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o maior artilheiro da história do Campeonato Brasileiro?',
+    'Roberto Dinamite'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o único clube brasileiro que nunca foi rebaixado para a segunda divisão?',
+    'Flamengo'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Brasil conquistou sua primeira Copa do Mundo?',
+    '1958'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas vezes Pelé venceu a Copa do Mundo como jogador?',
+    '3 vezes'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual estádio brasileiro é conhecido como o “Maracanã do Interior”?',
+    'Estádio Brinco de Ouro (Guarani)'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o técnico do Brasil na Copa do Mundo de 2002?',
+    'Luiz Felipe Scolari'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual jogador é o maior artilheiro da história do Santos?',
+    'Pelé'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual clube é conhecido como “O Galo”?',
+    'Atlético Mineiro'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Campeonato Brasileiro foi disputado pela primeira vez?',
+    '1971'
+)
